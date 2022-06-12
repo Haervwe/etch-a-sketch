129c2ffa435dd93e27f56516781005d9f169c5cd
Haervwe <juan-jose-007@hotmail.com>
2022-06-11 23:09:10 -0300

    add color selection,rainbow , reset and erase  functions

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
-let color = "#ffffff";
-
+let color = `#000000`;
+let gameMode = "standar";
 
 const board = document.querySelector(".board");
 const size = document.querySelector(".board").clientHeight;
 const sizeInput = document.querySelector("#size");
 const clrInput = document.querySelector("#clr");
+const resetBtn = document.querySelector(".reset");
+const rainbow = document.querySelector(".random");
+const erase = document.querySelector(".erase");
 
 let mouseDown = "false";
 document.body.onmousedown = () => (mouseDown="true");
 document.body.onmouseup = () => (mouseDown="false");
 
 // funtion that bills the board with divs //
+
+function randomNum (n) {
+    return Math.floor(Math.random()*n);
+}
 
 function fillBoard(n){
     let nSquare = n*n;
@@ -25,21 +32,42 @@ function fillBoard(n){
     }
 }
 
-
 // function to color with a color //
 
 function paint (e) { 
     if (e.type === "mouseover" && mouseDown == "false")return
-    else{
-        console.log(mouseDown);
-        e.target.style.backgroundColor = "black";
+    else if(gameMode == "standar"){
+        e.target.style.backgroundColor = color;
+        board.style.backgroundColor ="white";
+    }
+    else if(gameMode == "rainbow"){
+        let red = randomNum (255);
+        let green = randomNum (255);
+        let blue = randomNum (255);
+        e.target.style.backgroundColor = `rgb(${red},${green},${blue})`;
+        board.style.backgroundColor ="white";
+    }
+    else if(gameMode == "erase"){
+        e.target.style.backgroundColor = "white";
         board.style.backgroundColor ="white";
     }
 }
 
-//function to add drawing functionality //
+function reset (){
+    const divs = document.querySelectorAll(".square");
+    divs.forEach(square => {
+        square.style.backgroundColor = "white";        
+    });
+}
+
+
+//event listener to add drawing functionality//
 
 board.addEventListener("mouseover", paint);
+rainbow.addEventListener("click",() => (gameMode="rainbow"));
+resetBtn.addEventListener("click",reset);
+erase.addEventListener("click",() => (gameMode="erase"));
+clrInput.addEventListener("change",function (event){gameMode="standar";color = event.target.value;});
 
 
 
