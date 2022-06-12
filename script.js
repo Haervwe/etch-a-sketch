919f60e386a9c3c1dc594caa18751355080be7d0
Haervwe <juan-jose-007@hotmail.com>
2022-06-12 00:51:53 -0300

    add size, darker and brighter functions

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-let color = `#000000`;
+let color = `rgb(0,0,0)`;
 let gameMode = "standar";
+let rows = 16;
 
 const board = document.querySelector(".board");
 const size = document.querySelector(".board").clientHeight;
@@ -8,6 +9,8 @@ const clrInput = document.querySelector("#clr");
 const resetBtn = document.querySelector(".reset");
 const rainbow = document.querySelector(".random");
 const erase = document.querySelector(".erase");
+const darker =document.querySelector(".darker");
+const brighter =document.querySelector(".brighter");
 
 let mouseDown = "false";
 document.body.onmousedown = () => (mouseDown="true");
@@ -20,6 +23,9 @@ function randomNum (n) {
 }
 
 function fillBoard(n){
+    if (n > 64){
+        n = 64;
+    }
     let nSquare = n*n;
     let b = ((size-9)/n);
     for (var i = 0; i < nSquare ; i++) {
@@ -27,6 +33,7 @@ function fillBoard(n){
         newDiv.className = "square";
         newDiv.style.height = `${b}px`;
         newDiv.style.width = `${b}px`;
+        newDiv.style.backgroundColor = "rgb(255,255,255)";
         board.appendChild(newDiv);
         console.log("\${b}px");    
     }
@@ -38,38 +45,99 @@ function paint (e) {
     if (e.type === "mouseover" && mouseDown == "false")return
     else if(gameMode == "standar"){
         e.target.style.backgroundColor = color;
-        board.style.backgroundColor ="white";
+        board.style.backgroundColor ="rgb(255,255,255)";
     }
     else if(gameMode == "rainbow"){
         let red = randomNum (255);
         let green = randomNum (255);
         let blue = randomNum (255);
         e.target.style.backgroundColor = `rgb(${red},${green},${blue})`;
-        board.style.backgroundColor ="white";
+        board.style.backgroundColor ="rgb(255,255,255)";
     }
     else if(gameMode == "erase"){
-        e.target.style.backgroundColor = "white";
-        board.style.backgroundColor ="white";
+        e.target.style.backgroundColor = "rgb(255,255,255)";
+        board.style.backgroundColor ="rgb(255,255,255)";
+    }
+    else if(gameMode=="darker"){
+        let oldColor = e.target.style.backgroundColor;
+        const arr = oldColor.split("(");
+        arr.shift();
+        let rgbColString = arr.toString();
+        const rgbCol =rgbColString.split(")");
+        rgbCol.pop();
+        const rgbfin = rgbCol.toString().split(",");
+        let red = parseInt(rgbfin[0]);
+        let green = parseInt(rgbfin[1]);
+        let blue = parseInt(rgbfin[2]);
+        red *=0.9;
+        green*=0.9;
+        blue*=0.9;
+        red = parseInt(red);
+        green = parseInt(green);
+        blue = parseInt(blue);
+        if (red<0){
+            red = "0";
+        }
+        if (blue<0){
+            blue= "0";
+        }
+        if (red<0){
+            red="0";
+        }
+        e.target.style.backgroundColor = `rgb(${red},${green},${blue})`;
+        board.style.backgroundColor ="rgb(255,255,255)";    
+    }
+    else if(gameMode=="brighter"){
+        let oldColor = e.target.style.backgroundColor;
+        const arr = oldColor.split("(");
+        arr.shift();
+        let rgbColString = arr.toString();
+        const rgbCol =rgbColString.split(")");
+        rgbCol.pop();
+        const rgbfin = rgbCol.toString().split(",");
+        let red = parseInt(rgbfin[0]);
+        let green = parseInt(rgbfin[1]);
+        let blue = parseInt(rgbfin[2]);
+        red *=1.1;
+        green*=1.1;
+        blue*=1.1;
+        red = parseInt(red);
+        green = parseInt(green);
+        blue = parseInt(blue);
+        if (red>255){
+            red = "255";
+        }
+        if (blue>255){
+            blue= "255";
+        }
+        if (red>255){
+            red="255";
+        }
+        e.target.style.backgroundColor = `rgb(${red},${green},${blue})`;
+        board.style.backgroundColor ="rgb(255,255,255)";    
     }
 }
 
 function reset (){
     const divs = document.querySelectorAll(".square");
     divs.forEach(square => {
-        square.style.backgroundColor = "white";        
+        square.style.backgroundColor = "rgb(255,255,255)";        
     });
 }
+
 
 
 //event listener to add drawing functionality//
 
 board.addEventListener("mouseover", paint);
+board.addEventListener("mousedown", paint);
 rainbow.addEventListener("click",() => (gameMode="rainbow"));
 resetBtn.addEventListener("click",reset);
 erase.addEventListener("click",() => (gameMode="erase"));
 clrInput.addEventListener("change",function (event){gameMode="standar";color = event.target.value;});
-
-
+sizeInput.addEventListener("change",function (){board.innerHTML="";fillBoard(sizeInput.value);});
+darker.addEventListener("click",() => (gameMode="darker"));
+brighter.addEventListener("click",() => (gameMode="brighter"));
 
 fillBoard(16);
 
